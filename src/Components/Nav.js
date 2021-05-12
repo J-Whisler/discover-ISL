5dcf487fefe73e16b4d9790013b474f16c3fdd87
J-Whisler <jwhisler521@gmail.com>
2021-05-12 19:04:14 -0400

    file clean up and Nav component and styling copied from an existing project

---
+++ src/Components/Nav.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+import "../styles/_nav.scss";
+
+const Nav = () => {
+  return (
+    <div className="nav-container">
+      <div className="nav-menu">
+        <i className="fas fa-bars"></i>
+      </div>
+      <div className="logo">
+        <div className="logo-top">Discover</div>
+        <div className="logo-bottom">ICELAND!</div>
+      </div>
+      <div className="icons-container">
+        <div className="social-icons">
+          <a
+            href="https://twitter.com/IcelandNatural"
+            target="_blank"
+            rel="noreferrer"
+            className="icon"
+          >
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/iceland/"
+            target="_blank"
+            rel="noreferrer"
+            className="icon"
+          >
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a
+            href="https://www.facebook.com/icelandnaturally/"
+            target="_blank"
+            rel="noreferrer"
+            className="icon"
+          >
+            <i className="fab fa-facebook"></i>
+          </a>
+        </div>
+        <div className="plane">
+          <a
+            href="https://www.expedia.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="icon plane-icon"
+          >
+            <i className="fas fa-plane-departure"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
