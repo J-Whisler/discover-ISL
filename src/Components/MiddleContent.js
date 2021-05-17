4a428d9542dc647584f971fa591dcc6baa954037
J-Whisler <jwhisler521@gmail.com>
2021-05-16 21:57:24 -0400

    todo tab and plan tab

--- src/Components/MiddleContent.js
+++ src/Components/MiddleContent.js
@@ -50,10 +50,37 @@ const MiddleContent = () => {
           </div>
         </div>
         <div className={active === 2 ? "main-content-2" : "main-content"}>
-          Main Content 2
+          <h2>To Do</h2>
+          <h4>Eylenda</h4>
+          <div className="main-content-2-bottom">
+            <p>
+              Iceland is known for having a vast selection of things to do
+              during your visit including visiting one of its many geothermal
+              spas, whale watching, crossing off "see the Northern Lights" off
+              your bucket list and much, much more! See more of the many great
+              things to do on your trip to Iceland now!
+              <i className="fas fa-arrow-right"></i>
+            </p>
+            <a href="https://guidetoiceland.is/best-of-iceland/top-12-things-to-do-in-iceland">
+              To Do Iceland!
+            </a>
+          </div>
         </div>
         <div className={active === 3 ? "main-content-3" : "main-content"}>
-          Main Content 3
+          <h2>Plan</h2>
+          <h4>Islandia!</h4>
+          <div className="main-content-3-bottom">
+            <p>
+              Plan out your trip to Iceland thoroughly before you go! If you
+              want to see and do as many of the fun things that Iceland has to
+              offer it could mean doing a bit of driving so planning out your
+              trip is a must! See advice for planning your trip to Iceland!{" "}
+              <i className="fas fa-arrow-right"></i>
+            </p>
+            <a href="https://wearetravelgirls.com/planning-iceland-trip-guide/">
+              Plan Iceland!
+            </a>
+          </div>
         </div>
       </main>
       {/* Right sidebar Content */}
